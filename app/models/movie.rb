b802d509394025e1da37430a98983888a6fb4478
KurtHsu <myohmy10420@gmail.com>
2018-05-03 23:09:36 +0800

    remove searchkick

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,7 @@
 class Movie < ApplicationRecord
-  searchkick
   belongs_to :user
   has_many :reviews
 
-  has_attached_file :image, styles: { medium: "300x300#" }
+  has_attached_file :image, styles: { medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
