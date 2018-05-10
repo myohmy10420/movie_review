932819443c829ac8e5326a89eb54341c6b016927
KurtHsu <myohmy10420@gmail.com>
2018-05-10 23:16:20 +0800

    Enhencement ransack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :set_ransack_argument
-
   protect_from_forgery with: :exception
-
-  def set_ransack_argument
-    @q = Movie.ransack(params[:q])
-  end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,18 @@
 class SearchController < ApplicationController
+  before_action :validate_search_key
+
   def index
-    @movies = @q.result(distinct: true)
+    @movies = Movie.ransack(@search_criteria).result(distinct: true)
+  end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
   end
 end
